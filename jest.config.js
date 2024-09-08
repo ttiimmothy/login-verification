c39c49c37331cb932a62232773262073bd55f3a0
ttiimmothy <dinosauli2006@gmail.com>
2024-09-08 02:29:03 -0400

    fix add coverage threshold in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,13 @@ export default {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
   coveragePathIgnorePatterns: ['/node_modules/', '/src/app/layout.tsx']
 };
