6335c23f5824a527975322d7b045dc92c07743b2
ttiimmothy <dinosauli2006@gmail.com>
2024-09-22 04:17:04 -0400

    feat add verification success page

---
+++ src/app/success/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="bg-slate-50 h-screen w-screen flex justify-center items-center">
+      <h2 className="font-medium text-lg">Verification success</h2>
+    </div>
+  )
+}
+
+export default page
