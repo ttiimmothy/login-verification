8714e8824570460325b96dd22623fce7154a3015
ttiimmothy <dinosauli2006@gmail.com>
2024-09-22 14:24:25 -0400

    feat update the success page design

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const page = () => {
   return (
     <div className="bg-neutral-900 h-screen w-screen flex justify-center items-center text-yellow-50">
-      <div className="border-gray-100 border shadow-md shadow-gray-100 p-4 rounded-sm">
-        <h2 className="font-medium text-lg">Verification success</h2>
+      <div className="shadow-sm shadow-slate-500 p-4 rounded-sm">
+        <h2 className="font-medium text-xl">Verification success</h2>
       </div>
     </div>
   )
